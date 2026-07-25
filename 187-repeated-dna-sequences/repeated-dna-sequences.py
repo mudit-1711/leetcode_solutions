65e1f90a5e7ee147eb3fe836d4624bf1ba39b940
Mudit Agrawal <muditagrawal06@gmail.com>
2026-07-25 19:01:39 +0530

    Time: 27 ms (54.96%) | Memory: 32 MB (35.67%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        ans = []
+        mpp = {}
+        if len(s) < 10:
+            return ans
+        for i in range(len(s) - 9):
+            temp = s[i:i + 10] 
+            mpp[temp] = mpp.get(temp, 0) + 1
+        for key, value in mpp.items():
+            if value > 1:
+                ans.append(key)
+        return ans
