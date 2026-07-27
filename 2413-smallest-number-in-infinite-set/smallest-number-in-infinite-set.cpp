f8ff18cb6ee966c2c51dbd9250ae323366515a1a
Mudit Agrawal <muditagrawal06@gmail.com>
2026-07-27 19:53:31 +0530

    Time: 27 ms (20.43%) | Memory: 54.3 MB (5.73%) - LeetSync

--- 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
@@ -1,34 +1,19 @@
 class SmallestInfiniteSet {
 public:
-    vector<int> nums;
+    set<int> s;
+
     SmallestInfiniteSet() {
         for (int i = 1; i <= 1000; i++)
-            nums.push_back(i);
+            s.insert(i);
     }
+
     int popSmallest() {
-        int mini = nums[0], idx = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < mini) {
-                mini = nums[i];
-                idx = i;
-            }
-        }
-        int r = nums[idx];
-        nums.erase(nums.begin() + idx);
-        return r;
+        int x = *s.begin();
+        s.erase(s.begin());
+        return x;
     }
+
     void addBack(int num) {
-        for (auto x : nums) {
-            if (x == num)
-                return;
-        }
-        nums.push_back(num);
+        s.insert(num);
     }
 };
-
-/**
- * Your SmallestInfiniteSet object will be instantiated and called as such:
- * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
- * int param_1 = obj->popSmallest();
- * obj->addBack(num);
- */
