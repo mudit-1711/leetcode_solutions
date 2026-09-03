d4c4c7d2e92895fbed9c2c4c9d0c18f1c5a85ed4
Mudit Agrawal <muditagrawal06@gmail.com>
2026-09-03 21:53:36 +0530

    Time: 3 ms (72.51%) | Memory: 166 MB (18.85%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -2,28 +2,18 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums) {
         int i,j,n=nums.size();
-        // int mn = *min_element(nums.begin(),nums.end());
-        int odd=0;
-        int mn_odd=INT_MAX,mn_even=INT_MAX;
+        //int odd=0;
+        int mn_odd=INT_MAX;
         for(i=0;i<n;i++){
-            //if(nums[i]==mn)mn_idx=i;
             if(nums[i]%2==1){
-                odd++;
+                //odd++;
                 mn_odd=min(mn_odd,nums[i]);
-            }else{
-                mn_even=min(mn_even,nums[i]);
             }
         }
-        int even=n-odd;
-        if(even==n||odd==n){
-            return true;
-        }
-        //odd-even=odd
-        //odd-odd=even
-        //even-even=even
-        // even -odd = odd
-        vector<int>temp=nums;
-        //even
+        // int even=n-odd;
+        // if(even==n||odd==n){
+        //     return true;
+        // }
         bool ok1=true;
         for(i=0;i<n;i++){
             if(nums[i]%2==0){
@@ -36,7 +26,6 @@ public:
             }
         }
         bool ok2 = true;
-        //odd
         for(i=0;i<n;i++){
             if(nums[i]%2==1){
                 continue;
